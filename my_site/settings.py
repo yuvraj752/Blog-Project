3662f18cc70e672caccc5eec259c1bd21ac4439e
yuvraj752 <yuvrajrana752@gmail.com>
2023-01-03 20:13:37 +0530

    commit

--- my_site/settings.py
+++ my_site/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "mysite/static"
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
